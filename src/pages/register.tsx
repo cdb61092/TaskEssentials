c61ad5bc5610b55adb427014b2b0115a419b414c
cdb61092 <36825464+cdb61092@users.noreply.github.com>
2023-03-12 15:36:18 -0700

    Connect register component to real values

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,30 +1,55 @@
-import { useRouter } from "next/router";
 import { api } from "../utils/api";
+import { useState } from "react";
 
 export default function Register() {
-    const { query } = useRouter();
-    const register = api.user.register.useMutation();
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+  const { mutate } = api.user.register.useMutation();
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        register.mutate({
-            email: 'test@test.com',
-            password: 'test',
-            name: 'test test'
-
-        });
-        
-    }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate({
+      email: formValues.email,
+      password: formValues.password,
+      name: formValues.name,
+    });
+  };
   return (
     <div>
       <h1>Register</h1>
       <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="email"/>
-      <input type="text" placeholder="password"/>
-      <input type="text" placeholder="password"/>
-      <input type="submit" placeholder="submit"/>
+        <input
+          type="text"
+          id="email"
+          placeholder="email"
+          value={formValues.email}
+          onChange={(e) =>
+            setFormValues({ ...formValues, email: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          id="password"
+          placeholder="password"
+          value={formValues.password}
+          onChange={(e) =>
+            setFormValues({ ...formValues, password: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          id="name"
+          placeholder="name"
+          value={formValues.name}
+          onChange={(e) =>
+            setFormValues({ ...formValues, name: e.target.value })
+          }
+        />
+        <input type="submit" placeholder="submit" />
       </form>
-
     </div>
   );
 }
