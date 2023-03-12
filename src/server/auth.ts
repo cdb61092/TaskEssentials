ff6ed449388d9db377145bf377a76501ca810fa3
cdb61092 <36825464+cdb61092@users.noreply.github.com>
2023-03-12 13:09:44 -0700

    Set up credentials provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,12 +4,10 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 import CredentialsProvider from "next-auth/providers/credentials";
-import argon2 from 'argon2';
+import argon2 from "argon2";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -27,11 +25,6 @@ declare module "next-auth" {
       // role: UserRole;
     } & DefaultSession["user"];
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
 /**
@@ -43,60 +36,52 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.AUTH_SECRET,
   session: {
     strategy: "jwt",
-
   },
   callbacks: {
     session({ session }) {
-      // if (session.user) {
-      //   session.user.name = user.name || '';
-      //   session.user.email = user.email || '';
-      //   // session.user.role = user.role; <-- put other properties on the session here
-      // }
       return session;
     },
-    jwt({token}) {
-      console.log(`in jwt ${token.toString()}`)
+    jwt({ token }) {
       return token;
-    }
+    },
   },
   adapter: PrismaAdapter(prisma),
   providers: [
     /**
      * ...add more providers here.
      *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
      * @see https://next-auth.js.org/providers/github
      */
     CredentialsProvider({
       // The name to display on the sign in form (e.g. 'Sign in with...')
-    name: 'Credentials',
-    // The credentials is used to generate a suitable form on the sign in page.
-    // You can specify whatever fields you are expecting to be submitted.
-    // e.g. domain, username, password, 2FA token, etc.
-    // You can pass any HTML attribute to the <input> tag through the object.
-    credentials: {
-      email: { label: "Email", type: "text", placeholder: "jsmith" },
-      password: {  label: "Password", type: "password" }
-    },
-    async authorize(credentials) {
-      const user = await prisma.user.findFirst({
-				where: {
-					email: credentials?.email
-				},
-			});
-      
-      const passwordValid = await argon2.verify(user?.password || '', credentials?.password || '');
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials?.email,
+          },
+        });
 
-      if (passwordValid) {
-        return user;
-      } 
+        const passwordValid = await argon2.verify(
+          user?.password || "",
+          credentials?.password || ""
+        );
 
-      return null;
-    }
-    })
+        if (passwordValid) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
   ],
 };
 
@@ -111,7 +96,6 @@ export const getServerAuthSession = (ctx: {
 }) => {
   return getServerSession(ctx.req, ctx.res, authOptions);
 };
-
 
 export type User = {
   id: string;
